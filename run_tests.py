0db25245b3310f83a459dfcde01241a4b897061a
Alex Opryshko <alexopryshko@yandex.ru>
2014-10-19 18:30:34 +0400

    launcher tests

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python2
 
+import sys
+import unittest
+from tests.selenium_tests import SeleniumTestsCase
+
 if __name__ == '__main__':
-    pass
+    suite = unittest.TestSuite((
+        unittest.makeSuite(SeleniumTestsCase),
+    ))
+    result = unittest.TextTestRunner().run(suite)
+    sys.exit(not result.wasSuccessful())
